9c3956f6c1c68dbe78679b1481f2600ebe231756
Marko Borovac <marko.borovac@fer.hr>
2015-12-03 22:21:56 +0100

    testing mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,6 @@ Rails.application.configure do
       port:             587,
       domain:           'gmail.com', #you can also use google.com
       authentication:   :plain,
-      enable_starttls:  true,
       user_name:        'tidder.heroku@gmail.com',
       password:         'donotreply'
     }
